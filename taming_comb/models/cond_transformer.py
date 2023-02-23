8fd4eaa779017a51c3aa474f7a558b52f2e30e14
巫 <ba2000211@gmail.com>
2023-02-23 16:02:28 +0800

    Step1_final

---
+++ taming_comb/models/cond_transformer.py
@@ -0,0 +1,299 @@
+import os, math
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import argparse, os, sys, datetime, glob, importlib
+
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
+def disabled_train(self, mode=True):
+    """Overwrite model.train with this function to make sure train/eval mode
+    does not change anymore."""
+    return self
+
+
+class Net2NetTransformer(nn.Module):
+    #f = None
+    #device = '0'
+    def __init__(self, transformer_config, first_stage_model_config,
+                 cond_stage_config, f_path, device, permuter_config=None,
+                 ckpt_path=None, ignore_keys=[],
+                 first_stage_key="image",
+                 cond_stage_key="depth",
+                 downsample_cond_size=-1,
+                 pkeep=1.0):
+
+        super().__init__()
+
+        self.f_path = f_path
+        self.device = device
+        self.init_first_stage_from_ckpt(first_stage_model_config)
+        self.init_cond_stage_from_ckpt(cond_stage_config)
+        if permuter_config is None:
+            permuter_config = {"target": "taming_comb.modules.transformer.permuter.Identity"}
+        self.permuter = instantiate_from_config(config=permuter_config)
+        self.transformer = instantiate_from_config(config=transformer_config)
+
+        if ckpt_path is not None:
+            self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
+        self.first_stage_key = first_stage_key
+        self.cond_stage_key = cond_stage_key
+        self.downsample_cond_size = downsample_cond_size
+        self.pkeep = pkeep
+
+    def init_from_ckpt(self, path, ignore_keys=list()):
+        sd = torch.load(path, map_location="cpu")["state_dict"]
+        for k in sd.keys():
+            for ik in ignore_keys:
+                if k.startswith(ik):
+                    self.print("Deleting key {} from state_dict.".format(k))
+                    del sd[k]
+        self.load_state_dict(sd, strict=False)
+        print(f"Restored from {path}")
+
+    def init_first_stage_from_ckpt(self, config):
+        model = instantiate_from_config(config)
+        if(os.path.isfile(self.f_path)):
+            print('load ' + self.f_path)
+            ck = torch.load(self.f_path, map_location=self.device)
+            model.load_state_dict(ck['model_state_dict'], strict=False)
+        model = model.eval()
+        model.train = disabled_train
+        self.first_stage_model = model
+
+    def init_cond_stage_from_ckpt(self, config):
+        model = instantiate_from_config(config)
+        model = model.eval()
+        model.train = disabled_train
+        self.cond_stage_model = model
+
+    #def forward(self, x, c):
+    def forward(self, x, c, label):
+        # one step to produce the logits
+        _, z_indices,_ = self.encode_to_z(x, label)
+        _, c_indices = self.encode_to_c(c)
+
+        if self.training and self.pkeep < 1.0:
+            mask = torch.bernoulli(self.pkeep*torch.ones(z_indices.shape,
+                                                         device=z_indices.device))
+            mask = mask.round().to(dtype=torch.int64)
+            r_indices = torch.randint_like(z_indices, self.transformer.config.vocab_size)
+            a_indices = mask*z_indices+(1-mask)*r_indices
+        else:
+            a_indices = z_indices
+
+
+        cz_indices = torch.cat((c_indices, a_indices), dim=1)
+
+        # target includes all sequence elements (no need to handle first one
+        # differently because we are conditioning)
+        target = z_indices
+        # make the prediction
+        logits, _ = self.transformer(cz_indices[:, :-1])#[:, :-1]
+        # cut off conditioning outputs - output i corresponds to p(z_i | z_{<i}, c)
+        logits = logits[:, c_indices.shape[1]-1:]
+
+        return logits, target
+
+    def top_k_logits(self, logits, k):
+        v, ix = torch.topk(logits, k)
+        out = logits.clone()
+        out[out < v[..., [-1]]] = -float('Inf')
+        return out
+
+    @torch.no_grad()
+    def sample(self, x, c, steps, temperature=1.0, sample=False, top_k=None,
+               callback=lambda k: None):
+        x = torch.cat((c,x),dim=1)
+        block_size = self.transformer.get_block_size()
+        assert not self.transformer.training
+        if self.pkeep <= 0.0:
+            # one pass suffices since input is pure noise anyway
+            assert len(x.shape)==2
+            noise_shape = (x.shape[0], steps-1)
+            #noise = torch.randint(self.transformer.config.vocab_size, noise_shape).to(x)
+            noise = c.clone()[:,x.shape[1]-c.shape[1]:-1]
+            x = torch.cat((x,noise),dim=1)
+            logits, _ = self.transformer(x)
+            # take all logits for now and scale by temp
+            logits = logits / temperature
+            # optionally crop probabilities to only the top k options
+            if top_k is not None:
+                logits = self.top_k_logits(logits, top_k)
+            # apply softmax to convert to probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution or take the most likely
+            if sample:
+                shape = probs.shape
+                probs = probs.reshape(shape[0]*shape[1],shape[2])
+                ix = torch.multinomial(probs, num_samples=1)
+                probs = probs.reshape(shape[0],shape[1],shape[2])
+                ix = ix.reshape(shape[0],shape[1])
+            else:
+                _, ix = torch.topk(probs, k=1, dim=-1)
+            # cut off conditioning
+            x = ix[:, c.shape[1]-1:]
+        else:
+            for k in range(steps):
+                callback(k)
+                assert x.size(1) <= block_size # make sure model can see conditioning
+                x_cond = x if x.size(1) <= block_size else x[:, -block_size:]  # crop context if needed
+                logits, _ = self.transformer(x_cond)
+                # pluck the logits at the final step and scale by temperature
+                logits = logits[:, -1, :] / temperature
+                # optionally crop probabilities to only the top k options
+                if top_k is not None:
+                    logits = self.top_k_logits(logits, top_k)
+                # apply softmax to convert to probabilities
+                probs = F.softmax(logits, dim=-1)
+                # sample from the distribution or take the most likely
+                if sample:
+                    ix = torch.multinomial(probs, num_samples=1)
+                else:
+                    _, ix = torch.topk(probs, k=1, dim=-1)
+                # append to the sequence and continue
+                x = torch.cat((x, ix), dim=1)
+            # cut off conditioning
+            x = x[:, c.shape[1]:]
+        return x
+
+    @torch.no_grad()
+    def encode_to_z(self, x, label):
+        quant_z, _, info, style = self.first_stage_model.encode(x, label)
+        #_, quant_z = self.first_stage_model.encode_content(x)
+        indices = info[2].view(quant_z.shape[0], -1)
+        indices = self.permuter(indices)
+        return quant_z, indices, style
+
+    @torch.no_grad()
+    def encode_to_c(self, c):
+        if self.downsample_cond_size > -1:
+            c = F.interpolate(c, size=(self.downsample_cond_size, self.downsample_cond_size))
+        quant_c, _, info = self.cond_stage_model.encode(c)
+        indices = info[2].view(quant_c.shape[0], -1)
+        return quant_c, indices
+
+    @torch.no_grad()
+    def decode_to_img(self, index, zshape, style, label):
+        index = self.permuter(index, reverse=True)
+        bhwc = (zshape[0],zshape[2],zshape[3],zshape[1])
+        quant_z = self.first_stage_model.quantize.get_codebook_entry(
+            index.reshape(-1), shape=bhwc)
+        if label == 1:
+            x = self.first_stage_model.decode_a(quant_z, style)
+        else:
+            x = self.first_stage_model.decode_b(quant_z, style)
+        return x
+
+    @torch.no_grad()
+    def log_images(self, batch, temperature=None, top_k=None, callback=None, lr_interface=False, **kwargs):
+        log = dict()
+
+        N = 4
+        if lr_interface:
+            x, c = self.get_xc(batch, N, diffuse=False, upsample_factor=8)
+        else:
+            x, c = self.get_xc(batch, N)
+        x = x.to(device=self.device)
+        c = c.to(device=self.device)
+
+        quant_z, z_indices = self.encode_to_z(x)
+        quant_c, c_indices = self.encode_to_c(c)
+
+        # create a "half"" sample
+        z_start_indices = z_indices[:,:z_indices.shape[1]//2]
+        index_sample = self.sample(z_start_indices, c_indices,
+                                   steps=z_indices.shape[1]-z_start_indices.shape[1],
+                                   temperature=temperature if temperature is not None else 1.0,
+                                   sample=True,
+                                   top_k=top_k if top_k is not None else 100,
+                                   callback=callback if callback is not None else lambda k: None)
+        x_sample = self.decode_to_img(index_sample, quant_z.shape)
+
+        # sample
+        z_start_indices = z_indices[:, :0]
+        index_sample = self.sample(z_start_indices, c_indices,
+                                   steps=z_indices.shape[1],
+                                   temperature=temperature if temperature is not None else 1.0,
+                                   sample=True,
+                                   top_k=top_k if top_k is not None else 100,
+                                   callback=callback if callback is not None else lambda k: None)
+        x_sample_nopix = self.decode_to_img(index_sample, quant_z.shape)
+
+        # det sample
+        z_start_indices = z_indices[:, :0]
+        index_sample = self.sample(z_start_indices, c_indices,
+                                   steps=z_indices.shape[1],
+                                   sample=False,
+                                   callback=callback if callback is not None else lambda k: None)
+        x_sample_det = self.decode_to_img(index_sample, quant_z.shape)
+
+        # reconstruction
+        x_rec = self.decode_to_img(z_indices, quant_z.shape)
+
+        log["inputs"] = x
+        log["reconstructions"] = x_rec
+
+        if self.cond_stage_key != "image":
+            cond_rec = self.cond_stage_model.decode(quant_c)
+            if self.cond_stage_key == "segmentation":
+                # get image from segmentation mask
+                num_classes = cond_rec.shape[1]
+
+                c = torch.argmax(c, dim=1, keepdim=True)
+                c = F.one_hot(c, num_classes=num_classes)
+                c = c.squeeze(1).permute(0, 3, 1, 2).float()
+                c = self.cond_stage_model.to_rgb(c)
+
+                cond_rec = torch.argmax(cond_rec, dim=1, keepdim=True)
+                cond_rec = F.one_hot(cond_rec, num_classes=num_classes)
+                cond_rec = cond_rec.squeeze(1).permute(0, 3, 1, 2).float()
+                cond_rec = self.cond_stage_model.to_rgb(cond_rec)
+            log["conditioning_rec"] = cond_rec
+            log["conditioning"] = c
+
+        log["samples_half"] = x_sample
+        log["samples_nopix"] = x_sample_nopix
+        log["samples_det"] = x_sample_det
+        return log
+
+    def get_input(self, key, batch):
+        x = batch[key]
+        if len(x.shape) == 3:
+            x = x[..., None]
+        x = x.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format)
+        if x.dtype == torch.double:
+            x = x.float()
+        return x
+
+    def get_c(self, c_init):
+        c = c_init
+        if len(c.shape) == 3:
+            c = c[..., None]
+        #c = c.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format)
+        c = c.permute(2, 3, 0, 1).to(memory_format=torch.contiguous_format)
+        if c.dtype == torch.double:
+            c = c.float()
+        return c
+
+
+    def get_xc(self, batch, N=None):
+        x = self.get_input(self.first_stage_key, batch)
+        c = self.get_input(self.cond_stage_key, batch)
+        if N is not None:
+            x = x[:N]
+            c = c[:N]
+        return x, c
---
+++ taming_comb/models/vqgan.py
@@ -0,0 +1,224 @@
+import torch
+import torch.nn.functional as F
+#import pytorch_lightning as pl
+import argparse, os, sys, datetime, glob, importlib
+import torch.nn as nn
+from torch.autograd import Variable
+
+from taming_comb.modules.diffusionmodules.model import * #Encoder, Decoder, VUNet
+from taming_comb.modules.vqvae.quantize import VectorQuantizer
+from taming_comb.modules.discriminator.model import NLayerDiscriminator
+from taming_comb.modules.losses.vqperceptual import hinge_d_loss
+from torch.utils.checkpoint import checkpoint
+
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
+class VQModel_ADAIN(nn.Module):
+    def __init__(self,
+                 ddconfig,
+                 lossconfig,
+                 n_embed,
+                 embed_dim,
+                 ckpt_path=None,
+                 ignore_keys=[],
+                 image_key="image",
+                 colorize_nlabels=None,
+                 monitor=None
+                 ):
+        super(VQModel_ADAIN, self).__init__()
+        self.image_key = image_key
+        self.encoder = Encoder(**ddconfig)
+        self.decoder_a = Decoder(**ddconfig)
+        self.decoder_b = Decoder(**ddconfig)
+        self.loss_a = instantiate_from_config(lossconfig)
+        self.loss_b = instantiate_from_config(lossconfig)
+        self.quantize = VectorQuantizer(n_embed, embed_dim, beta=0.25)
+        self.quant_conv = torch.nn.Conv2d(ddconfig["z_channels"], embed_dim, 1)
+        self.post_quant_conv = torch.nn.Conv2d(embed_dim, ddconfig["z_channels"], 1)
+        
+        
+        #style encoders
+#        self.style_enc_a = StyleEncoder(4, 3, 64, 8, norm='none', activ='relu', pad_type='reflect')
+        
+#        self.style_enc_b = StyleEncoder(4, 3, 64, 8, norm='none', activ='relu', pad_type='reflect')
+        
+        # MLP to generate AdaIN parameters
+#        n_a = self.get_num_adain_params(self.decoder_a)
+#        n_b = self.get_num_adain_params(self.decoder_b)
+        
+#        self.mlp_a = MLP(8, n_a, 256, 3, norm='none', activ='relu')  
+#        self.mlp_b = MLP(8, n_b, 256, 3, norm='none', activ='relu')
+        
+
+
+    def encode(self, x):
+        h = self.encoder(x)
+        h = self.quant_conv(h)
+        quant, emb_loss, info = self.quantize(h)
+        
+        #encode style
+#        if label == 1:
+#            style_encoded = self.style_enc_a(x)
+#        else:
+#            style_encoded = self.style_enc_b(x)
+            
+#        style_encoded = style_encoded
+         
+        return quant, emb_loss,# info, #style_encoded
+    
+    
+    def encode_style(self, x, label):
+        if label == 1:
+            style_encoded = self.style_enc_a(x)
+        else:
+            style_encoded = self.style_enc_b(x)
+            
+        return style_encoded
+    
+    
+    def encode_content(self, x):
+        h = self.encoder(x)
+        h = self.quant_conv(h)
+        quant, _, _ = self.quantize(h)
+        return h, quant
+            
+    
+
+    def decode_a(self, quant):
+        # decode content and style codes to an image
+#        self.mlp_a = self.mlp_a.to(style_a.device)
+#        adain_params = self.mlp_a(style_a)
+#        self.assign_adain_params(adain_params, self.decoder_a)
+ 
+        
+        quant = self.post_quant_conv(quant)
+        
+        dec = self.decoder_a(quant)
+        return dec
+
+    def decode_b(self, quant):
+        # decode content and style codes to an image
+#        self.mlp_b = self.mlp_b.to(style_b.device)
+#        adain_params = self.mlp_b(style_b)
+#        self.assign_adain_params(adain_params, self.decoder_b)
+        
+        quant = self.post_quant_conv(quant)
+        dec = self.decoder_b(quant)
+        return dec
+
+ 
+    def forward(self, input, label):
+        if(label == 1):
+            quant, diff, _ = self.encode(input, label)     #uant, diff, _, style_encoded = self.encode(input, label)
+            dec = self.decode_a(quant)
+        else:
+            quant, diff, _ = self.encode(input, label)
+            dec = self.decode_b(quant)
+
+        return dec, diff
+
+
+    def get_last_layer(self, label):
+        if(label ==  1):
+            return self.decoder_a.conv_out.weight
+        else:
+            return self.decoder_b.conv_out.weight
+
+    def assign_adain_params(self, adain_params, model):
+        # assign the adain_params to the AdaIN layers in model
+        for m in model.modules():
+            if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
+                mean = adain_params[:, :m.num_features]
+                std = adain_params[:, m.num_features:2*m.num_features]
+                m.bias = mean.contiguous().view(-1).to(adain_params.device)
+                m.weight = std.contiguous().view(-1).to(adain_params.device)
+                if adain_params.size(1) > 2*m.num_features:
+                    adain_params = adain_params[:, 2*m.num_features:]
+
+        
+    def get_num_adain_params(self, model):
+        # return the number of AdaIN parameters needed by the model
+        num_adain_params = 0
+        for m in model.modules():
+            if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
+                num_adain_params += 2*m.num_features
+        return num_adain_params
+
+    
+
+class VQModelCrossGAN_ADAIN(VQModel_ADAIN):
+    def __init__(self,
+                 ddconfig,
+                 lossconfig,
+                 n_embed,
+                 embed_dim,
+                 ckpt_path=None,
+                 ignore_keys=[],
+                 image_key="image",
+                 colorize_nlabels=None,
+                 monitor=None,
+                 ):
+        super(VQModelCrossGAN_ADAIN, self).__init__(
+            ddconfig, lossconfig, n_embed, embed_dim
+        )
+
+    def forward(self, input, label, cross=False, s_given=False,use_checkpoint=True):
+        input = Variable(input,requires_grad=True)
+        if use_checkpoint:
+            quant, diff= checkpoint(self.encode, input)
+
+            # sample
+            '''s_a = Variable(torch.randn(input.size(0), 8, 1, 1).cuda())
+            s_b = Variable(torch.randn(input.size(0), 8, 1, 1).cuda())'''
+
+            if(label == 1):
+                if cross == False:
+                    output = checkpoint(self.decode_a , quant)
+                else:
+                    s = s_given
+                    output = checkpoint(self.decode_b ,quant)
+            else:
+                if cross == False:
+                    output = checkpoint(self.decode_b , quant)
+                else:
+                    s = s_given
+                    output = checkpoint(self.decode_a , quant)
+        else:
+            quant, diff= self.encode(input)
+            
+            # sample
+            '''s_a = Variable(torch.randn(input.size(0), 8, 1, 1).cuda())
+            s_b = Variable(torch.randn(input.size(0), 8, 1, 1).cuda())'''
+
+            if(label == 1):
+                if cross == False:
+                    output = self.decode_a(quant)
+                else:
+                    s = s_given
+                    output = self.decode_b(quant)
+            else:
+                if cross == False:
+                    output = self.decode_b(quant)
+                else:
+                    s = s_given
+                    output = self.decode_a(quant)
+
+        return output, diff
+
+    
+
+
